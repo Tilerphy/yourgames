d138e62634af44c65173bf336816a43837c8863f
root <root@homeserver.hs.org>
2016-03-25 13:49:03 +0800

    change the data to mysql adapter

--- ad/ad.sql
+++ ad/ad.sql
@@ -1,3 +1,6 @@
+set collation_connection=utf9_general_ci;
+set collation_database=utf9_general_ci;
+set collation_server=utf9_general_ci;
 
 create table item (id varchar(64) primary key, identifier varchar(100), slidetime int,title varchar(100), phone varchar(200), address varchar(200), description varchar(500), typename varchar(20), position varchar(20));
 
