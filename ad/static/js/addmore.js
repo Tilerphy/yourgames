6d0992ad80fef4d28534f1d406de02c390556e73
root <root@homeserver.hs.org>
2016-04-01 10:14:10 +0800

    add marks of sales

--- ad/static/js/addmore.js
+++ ad/static/js/addmore.js
@@ -1,14 +1,19 @@
+var data = {};
 function reload() {
        $.get("/data?p="+load_position+"&t="+load_typename, function(result){
             var items = result[load_typename];
+            data= {};
             $("#owner").html("");
             for(var key in items){
                 $("#owner").append($("<option value='"+items[key].id+"'>"+items[key].detail.title+"</option>"));
+                data[items[key].id] = {description: items[key].detail.description, slidetime: items[key].slidetime};
             }
-            $("#description").text(items[key].detail.description);
        });
 }
-
+function ownerChanged(args) {
+    $("#slidetime").val(data[args.value].slidetime);
+    $("#description").text(data[args.value].description);
+}
 function typenameChanged(args) {
     load_typename= args.value;
     reload();
